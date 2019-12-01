7302f7acf259a740fe5480f15341582b060b5c10
Nazar Kostyk <kostyknazar1@gmail.com>
2019-12-01 16:56:54 +0200

    New version created

---
+++ Monny/HomePage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Monny
+{
+	/// <summary>
+	/// Interaction logic for HomePage.xaml
+	/// </summary>
+	public partial class HomePage : Page
+	{
+		private MainWindow controller;
+		public HomePage(MainWindow _mainWindow)
+		{
+			InitializeComponent();
+			controller = _mainWindow;
+		}
+		private void OpenMenuButton_Click(object sender, RoutedEventArgs e)
+		{
+			OpenMenuButton.Visibility = Visibility.Collapsed;
+			CloseMenuButton.Visibility = Visibility.Visible;
+		}
+		private void CloseMenuButton_Click(object sender, RoutedEventArgs e)
+		{
+			OpenMenuButton.Visibility = Visibility.Visible;
+			CloseMenuButton.Visibility = Visibility.Collapsed;
+		}
+	}
+}
