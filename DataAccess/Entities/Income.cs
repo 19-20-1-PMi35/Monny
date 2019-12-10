22113648040f7d8babc85cf9db6df2767773a460
oksayar <oksyarysh@gmail.com>
2019-12-10 06:05:42 +0200

    Creating an entity for Income Page

---
+++ DataAccess/Entities/Income.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Entities
+{
+    public class Income
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double MoneyCount { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
--- DataAccess/MonnyDbContext.cs
+++ DataAccess/MonnyDbContext.cs
@@ -13,6 +13,7 @@ namespace DataAccess
         public DbSet<Category> Categories { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Income> Incomes { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -24,7 +25,13 @@ namespace DataAccess
                 new Category {Id = 4, Name = "Clothes"},
                 new Category {Id = 5, Name = "Cafes"},
                 new Category {Id = 6, Name = "Transport"},
-                new Category {Id = 7, Name = "Entertainments"}
+                new Category {Id = 7, Name = "Entertainments"},
+                new Category {Id = 8, Name = "Salary", CategoryType = 1},
+                new Category {Id = 9, Name = "Scholarship", CategoryType = 1},
+                new Category {Id = 10, Name = "Retirement", CategoryType = 1},
+                new Category {Id = 11, Name = "Rent", CategoryType = 2},
+                new Category {Id = 12, Name = "Part-time job", CategoryType = 2},
+                new Category {Id = 13, Name = "Shares", CategoryType = 2}
             };
 
             modelBuilder.Entity<Category>().HasData(categories);
